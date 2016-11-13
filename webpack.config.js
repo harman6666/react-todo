2d38aff5b6ca3d4b2e36ed9ebd05c8dff9b07e7e
Harmanpreet singh <hsin58@sapient.com>
2016-11-13 13:38:08 +0530

    Todo example with react

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	devtool: 'inline-source-map',
+	entry: [
+		'webpack-dev-server/client?http://127.0.0.1:8080/',
+		'webpack/hot/only-dev-server',
+		'./src'
+	],
+	output: {
+		path: path.resolve(__dirname, "public"),
+		filename: 'bundle.js'
+	},
+	resolve: {
+		moduleDirectories: ['node_modules', 'src'],
+		extensions: ['', '.js']
+	},
+	module: {
+		loaders:[
+			{
+				test: /\.jsx?$/,
+				loader: 'babel',
+				exclude: /node_modules/,
+				query: {
+		        	presets: ['react', 'es2015']
+		        }
+			}
+		]
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin()
+	]
+}
